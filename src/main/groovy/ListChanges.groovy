775811312ec5952996d436ccedb45bd0f963dd17
Martin Malmsten <martin.malmsten@kb.se>
2016-03-21 16:16:44 +0100

    First version of ListRecords.groovy. It's not pretty, but it works

--- src/main/groovy/ListChanges.groovy
+++ src/main/groovy/ListChanges.groovy
@@ -10,6 +10,7 @@ profile = new ExportProfile(new File("etc/export.properties"))
 config = new ConfigSlurper().parse(new File("etc/config.properties").toURL())
 
 def get(url) {
+  //System.err.println "DEBUG: " + url
   def conn = url.toURL().openConnection()
 
   if (config.Password != "") {
@@ -18,6 +19,57 @@ def get(url) {
   }
 
   return conn.content.text
+}
+
+def listAuthIdentifiers(from, until) {
+  def ret = new TreeSet<Integer>()
+  def data = new XmlSlurper(false, false).parseText(get("${config.OaiPmhBaseUrl}auth/oaipmh?verb=ListIdentifiers&metadataPrefix=marcxml&from=${from}&until=${until}"))
+
+  def token = null
+  while (token != "") {
+    data.ListIdentifiers.header.identifier.each { id ->
+      def auth_id = id.toString().split('/')[-1]
+      def data2 = new XmlSlurper(false, false).parseText(get("${config.OaiPmhBaseUrl}bib/oaipmh?verb=ListIdentifiers&metadataPrefix=marcxml&set=authority:${auth_id}"))
+
+      def token2 = null
+      while (token2 != "") {
+        data2.ListIdentifiers.header.each { header ->
+          def bib_id = id.toString().split('/')[-1]
+          def out = false;
+
+          header.setSpec.each { setSpec ->
+            def set = setSpec.toString()
+            if (!out && (set.startsWith("location:") && set.substring(9) in profile.getSet("locations") || "*" in profile.getSet("locations"))) {
+              if (header.@status.toString().equals("deleted")) {
+                  //System.err.println("# DELETED: " + id)
+              } else {
+                out = true
+                ret.add(Integer.parseInt(bib_id))
+              }
+            }
+          }
+        }
+
+        token2 = data2.ListIdentifiers.resumptionToken.size() == 0? "":data2.ListIdentifiers.resumptionToken.toString()
+
+        if (!token2.equals("")) {
+          //System.err.println("DEBUG: resumptionToken=${token}")
+          data2 = new XmlSlurper(false, false).parseText(get("${config.OaiPmhBaseUrl}bib/oaipmh?verb=ListIdentifiers&resumptionToken=${token2}"))
+        }
+      }
+    }
+
+    token = data.ListRecords.resumptionToken.size() == 0? "":data.ListRecords.resumptionToken.toString()
+
+    if (!token.equals("")) {
+      //System.err.println("DEBUG: resumptionToken=${token}")
+      data = new XmlSlurper(false, false).parseText(get("${config.OaiPmhBaseUrl}auth/oaipmh?verb=ListIdentifiers&resumptionToken=${token}"))
+    }
+  }
+
+  System.err.println "DEBUG: found ${ret.size()} bib records with changes in related auth records"
+
+  return ret
 }
 
 def listBibIdentifiers(from, until) {
@@ -42,7 +94,7 @@ def listBibIdentifiers(from, until) {
           def set = setSpec.toString()
           if (!out && (set.startsWith("location:") && set.substring(9) in profile.getSet("locations") || "*" in profile.getSet("locations"))) {
             if (header.@status.toString().equals("deleted")) {
-                System.err.println("# DELETED: " + id)
+                //System.err.println("# DELETED: " + id)
             } else {
               out = true
               ret.add(Integer.parseInt(id))
@@ -54,10 +106,12 @@ def listBibIdentifiers(from, until) {
       }
 
       token = data.ListIdentifiers.resumptionToken.size() == 0? "":data.ListIdentifiers.resumptionToken.toString()
-      System.err.println("DEBUG: resumptionToken=${token}")
+      //System.err.println("DEBUG: resumptionToken=${token}")
       data = new XmlSlurper(false, false).parseText(get("${config.OaiPmhBaseUrl}bib/oaipmh?verb=ListIdentifiers&resumptionToken=${token}"))
     }
   }
+
+  System.err.println "DEBUG: found ${ret.size()} updated and/or created bib records"
 
   return ret
 }
@@ -84,23 +138,26 @@ def listHoldIdentifiers(from, until) {
         header.setSpec.each { setSpec ->
           def set = setSpec.toString()
           if (set.startsWith("bibid:")) bibid = set.substring(6)
-
-          if (header.@status.toString().equals("deleted")) {
-            deleted = true
-            System.err.println("# DELETED: " + id)
-          }
         }
 
-        if (!deleted) ret.add(Integer.parseInt(bibid))
+        if (header.@status.toString().equals("deleted")) {
+          deleted = true
+        }
 
-        //if (!out) System.err.println("SKIPPED: " + id)
+        if (header.@status.toString().equals("deleted")) {
+          //System.err.println("DEBUG: DELETED location:${location} holding_id:${id} bib_id:${bibid}")
+        } else {
+          ret.add(Integer.parseInt(bibid))
+        }
       }
 
       token = data.ListIdentifiers.resumptionToken.size() == 0? "":data.ListIdentifiers.resumptionToken.toString()
-      System.err.println("DEBUG: resumptionToken=${token}")
+      //System.err.println("DEBUG: resumptionToken=${token}")
       data = new XmlSlurper(false, false).parseText(get("${config.OaiPmhBaseUrl}hold/oaipmh?verb=ListIdentifiers&resumptionToken=${token}"))
     }
   }
+
+  System.err.println "DEBUG: found ${ret.size()} bib records with changes in related holding records"
 
   return ret
 }
@@ -108,6 +165,7 @@ def listHoldIdentifiers(from, until) {
 def ids = new TreeSet<Integer>()
 
 ids.addAll(listBibIdentifiers("2016-03-01T00:00:00Z", "2016-03-01T23:59:59Z"))
+ids.addAll(listAuthIdentifiers("2016-03-01T00:00:00Z", "2016-03-01T23:59:59Z"))
 ids.addAll(listHoldIdentifiers("2016-03-01T00:00:00Z", "2016-03-01T23:59:59Z"))
 
 for (Integer id: ids)
