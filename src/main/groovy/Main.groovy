1947a908cc439acbac89bf842a2fab68552212ad
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2017-07-14 12:49:42 +0200

    Add the ability to run the export program as a precompiled JAR (as opposed to only as a gradle task).

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,34 @@
+/**
+ * A wrapper for the various tasks, allowing them to be run from a prebuilt jar
+ * without gradle, for performance reasons.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        if (args.length < 1) {
+            println("Please pass a task as a parameter (for example \"ListChanges_xl\") at the end of your command.")
+            return
+        }
+
+        String[] filteredArgs = new String[args.length-1]
+        for (int i = 0; i < args.length-1; ++i)
+            filteredArgs[i] = args[i+1]
+
+        switch (args[0]) {
+            case "GetRecords":
+                GetRecords.main(filteredArgs)
+                break
+            case "ListChanges":
+                ListChanges.main(filteredArgs)
+                break
+            case "GetRecords_xl":
+                GetRecords_xl.main(filteredArgs)
+                break
+            case "ListChanges_xl":
+                ListChanges_xl.main(filteredArgs)
+                break
+        }
+    }
+
+}
