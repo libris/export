86409876fa2388506e5596b4e1409242b24394ac
Jannis Mohlin Tsiroyannis <jantsi@kb.local>
2018-10-08 12:51:36 +0200

    Update windows example script to use server side exports.

--- examplescripts/export_windows.bat
+++ examplescripts/export_windows.bat
@@ -1,21 +1,13 @@
-@echo off
+﻿@echo off
 
 rem Det här skriptet kan användas som exempel på hur man automatiskt hämtar poster från Libris
-rem Innan du använder det, se till att du fyllt i filerna:
-rem etc/config_xl.properties
-rem etc/export.properties
+rem Innan du använder det, se till att du fyllt i filen: etc/export.properties
 rem Lämpligen körs detta skript minut-vis som ett schemalagt jobb. SE TILL ATT DET INTE STARTAS INNAN FÖREGÅENDE KÖRNING ÄR KLAR!
-
-rem Om exportprogrammet inte kompilerats än
-set jarpath=build/libs/export-3.0.0-alpha.jar
-IF NOT EXIST %jarpath% (
-	gradlew.bat jar
-)
 
 rem Om vi kör för första gången, använd 'nu' som start-tid.
 IF NOT EXIST lastRun.timestamp (
         CALL :LOADUTCNOW
-	echo %currentTime%>lastRun.timestamp
+	@echo %currentTime%>lastRun.timestamp
 )
 
 rem Avgör tidsintervall
@@ -23,10 +15,8 @@ set /p startTime=<lastRun.timestamp
 CALL :LOADUTCNOW
 set stopTime=%currentTime%
 
-java -jar %jarpath% ListChanges_xl -Prange^=%startTime%,%stopTime% > bibids
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-java -jar %jarpath% GetRecords_xl > export.txt < bibids
+# Hämta data
+curl --fail -XPOST "https://libris.kb.se/api/marc_export/?from=%startTime%&until=%stopTime%&deleted=ignore&virtualDelete=false" --data-binary @.\etc\export.properties > export.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem Om allt gick bra, uppdatera tidsstämpeln
